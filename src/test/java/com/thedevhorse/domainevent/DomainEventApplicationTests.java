dbc254675b4c9a331c932e7a42c78f1718d8af69
hugop <hugopessego@gmail.com>
2024-11-19 14:40:45 +0000

    Add initial Spring Boot project setup
    This commit introduces the initial setup for a Spring Boot project. It includes core files such as `pom.xml`, configuration files, and basic application structure. Each IDE's specific settings and Maven wrapper scripts are also incorporated for easier development.

---
+++ src/test/java/com/thedevhorse/domainevent/DomainEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedevhorse.domainevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
