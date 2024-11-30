2b13a29d4794fde09f593ebfcf75c5fc4ac67f81
hugop <hugopessego@gmail.com>
2024-11-30 07:02:43 +0000

    Enhance messaging with Kafka integration
    Add Spring Cloud Stream with Kafka support to facilitate message publishing. Introduce dependency management for consistency and set up docker-compose for local Kafka/Zookeeper services. Additionally, implement a reactive sink in OrderMessagingProducer for domain events, preparing the codebase for event-driven architecture.

--- src/main/java/com/thedevhorse/domainevent/messaging/OrderMessagingProducer.java
+++ src/main/java/com/thedevhorse/domainevent/messaging/OrderMessagingProducer.java
@@ -1,13 +1,25 @@
 package com.thedevhorse.domainevent.messaging;
 
 import com.thedevhorse.domainevent.domain.DomainEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+
+import java.util.function.Supplier;
 
 @Component
 public class OrderMessagingProducer implements OrderMessagePublisher {
 
+    Sinks.Many<DomainEvent> sinks = Sinks.many().replay().latest();
+
+    @Bean
+    public Supplier<Flux<DomainEvent>> orderSupplier(){
+        return ()-> sinks.asFlux();
+    }
+
     @Override
     public void producer(DomainEvent domainEvent) {
-
+        sinks.tryEmitNext(domainEvent);
     }
 }
